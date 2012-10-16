fc790a79d951e074af5013ca005e9fd1515d92da
Marciano Siniscalchi <msiniscalchi@gmail.com>
2012-10-15 22:33:12 -0500

    First stab at robust, recursive parseTeXlog

--- makePDF.py
+++ makePDF.py
@@ -4,6 +4,7 @@ import subprocess
 import types
 import re
 import getTeXRoot
+import parseTeXlog
 
 DEBUG = False
 
@@ -24,244 +25,6 @@ def getOEMCP():
     codepage = ctypes.windll.kernel32.GetOEMCP()
     return str(codepage)
 
-
-# Log parsing, TNG :-)
-# Input: tex log file (decoded), split into lines
-# Output: content to be displayed in output panel, split into lines
-
-def parseTeXlog(log):
-	print "Parsing log file"
-	errors = []
-	warnings = []
-	
-	# loop over all log lines; construct error message as needed
-	# This will be useful for multi-file documents
-
-	# some regexes
-	file_rx = re.compile(r"\(([^)]+)$")		# file name: "(" followed by anyting but "(" through the end of the line
-	line_rx = re.compile(r"^l\.(\d+)\s(.*)")		# l.nn <text>
-	warning_rx = re.compile(r"^(.*?) Warning: (.+)") # Warnings, first line
-	line_rx_latex_warn = re.compile(r"input line (\d+)\.$") # Warnings, line number
-	matched_parens_rx = re.compile(r"\([^()]*\)") # matched parentheses, to be deleted (note: not if nested)
-	assignment_rx = re.compile(r"\\[^=]*=")	# assignment, heuristics for line merging
-
-	files = []
-
-	# Support function to handle warnings
-	def handle_warning(l):
-
-		if files==[]:
-			location = "[no file]"
-			errors.append("LaTeXTools cannot correctly detect file names in this LOG file.")
-			errors.append("Please let me know via GitHub. Thanks!")
-		else:
-			location = files[-1]		
-
-		warn_match_line = line_rx_latex_warn.search(l)
-		if warn_match_line:
-			warn_line = warn_match_line.group(1)
-			warnings.append(location + ":" + warn_line + ": " + l)
-		else:
-			warnings.append(location + ": " + l)
-
-	
-	# State definitions
-	STATE_NORMAL = 0
-	STATE_SKIP = 1
-	STATE_REPORT_ERROR = 2
-	STATE_REPORT_WARNING = 3
-	
-	state = STATE_NORMAL
-
-	# Use our own iterator instead of for loop
-	log_iterator = log.__iter__()
-	line_num=0
-	line = ""
-
-	recycle_extra = False # just in case
-
-	while True:
-		# first of all, see if we have a line to recycle (see heuristic for "l.<nn>" lines)
-		if recycle_extra:
-			line = extra
-			#print "Recycling line"
-			recycle_extra = False
-		else:
-			# save previous line for "! File ended while scanning use of..." message
-			prev_line = line
-			try:
-				line = log_iterator.next() # will fail when no more lines
-			except StopIteration:
-				break
-		line_num += 1
-		# Now we deal with TeX's decision to truncate all log lines at 79 characters
-		# If we find a line of exactly 79 characters, we add the subsequent line to it, and continue
-		# until we find a line of less than 79 characters
-		# The problem is that there may be a line of EXACTLY 79 chars. We keep our fingers crossed but also
-		# use some heuristics to avoid disastrous consequences
-		# We are inspired by latexmk (which has no heuristics, though)
-
-		# HEURISTIC: the first line is always long, and we don't care about it
-		# also, the **<file name> line may be long, but we skip it, too (to avoid edge cases)
-		if line_num>1 and len(line)>=79 and line[0:2] != "**": 
-			# print "Line %d is %d characters long; last char is %s" % (line_num, len(line), line[-1])
-			# HEURISTICS HERE
-			extend_line = True
-			recycle_extra = False
-			while extend_line:
-				try:
-					extra = log_iterator.next()
-					line_num += 1 # for debugging purposes
-					# HEURISTIC: if extra line begins with "Package:" "File:" "Document Class:",
-					# or other "well-known markers",
-					# we just had a long file name, so do not add
-					if len(extra)>0 and \
-					   (extra[0:5]=="File:" or extra[0:8]=="Package:" or extra[0:15]=="Document Class:") or \
-					   (extra[0:9]=="LaTeX2e <") or assignment_rx.match(extra):
-						extend_line = False
-						# no need to recycle extra, as it's nothing we are interested in
-					# HEURISTIC: when TeX reports an error, it prints some surrounding text
-					# and may use the whole line. Then it prints "...", and "l.<nn> <text>" on a new line
-					# If so, do not extend
-					elif line[-3:]=="..." and line_rx.match(extra): # a bit inefficient as we match twice
-						#print "Found l. <nn> regex"
-						extend_line = False
-						recycle_extra = True # make sure we process the "l.<nn>" line!
-					else:
-						line += extra
-						if len(extra) < 79:
-							extend_line = False
-				except StopIteration:
-					extend_line = False # end of file, so we must be done. This shouldn't happen, btw
-		# Check various states
-		if state==STATE_SKIP:
-			state = STATE_NORMAL
-			continue
-		if state==STATE_REPORT_ERROR:
-			# skip everything except "l.<nn> <text>"
-			print line
-			err_match = line_rx.match(line)
-			if not err_match:
-				continue
-			# now we match!
-			state = STATE_NORMAL
-			err_line = err_match.group(1)
-			err_text = err_match.group(2)
-			# err_msg is set from last time
-			if files==[]:
-				location = "[no file]"
-				errors.append("LaTeXTools cannot correctly detect file names in this LOG file.")
-				errors.append("Please let me know via GitHub. Thanks!")
-			else:
-				location = files[-1]		
-			errors.append(location + ":" + err_line + ": " + err_msg + " [" + err_text + "]")
-			continue
-		if state==STATE_REPORT_WARNING:
-			# add current line and check if we are done or not
-			current_warning += line
-			if line[-1]=='.':
-				handle_warning(current_warning)
-				current_warning = None
-				state = STATE_NORMAL # otherwise the state stays at REPORT_WARNING
-			continue
-		if line=="":
-			continue
-		# Remove matched parentheses: they do not add new files to the stack
-		# Do this iteratatively; for instance, on Windows 64, miktex has some files in
-		# "Program Files (x86)", which wreaks havoc
-		# NOTE: this means that your file names CANNOT have parentheses!!!
-		while True:
-			line_purged = matched_parens_rx.sub("", line)
-			# if line != line_purged:
-				# print "Purged parens on line %d:" % (line_num, )  
-				# print line
-				# print line_purged
-			if line != line_purged:
-				line = line_purged
-			else:
-				break
-		# Special error reporting for e.g. \footnote{text NO MATCHING PARENS & co
-		if "! File ended while scanning use of" in line:
-			scanned_command = line[35:-2] # skip space and period at end
-			# we may be unable to report a file by popping it, so HACK HACK HACK
-			file_name = log_iterator.next() # <inserted text>
-			file_name = log_iterator.next() #      \par
-			file_name = log_iterator.next()[3:] # here is the file name with <*> in front
-			errors.append("TeX STOPPED: " + line[2:-2]+prev_line[:-5])
-			errors.append("TeX reports the error was in file:" + file_name)
-			continue
-		# Here, make sure there was no uncaught error, in which case we do more special processing
-		if "!  ==> Fatal error occurred, no output" in line:
-			if errors == []:
-				errors.append("TeX STOPPED: fatal errors occurred but LaTeXTools did not see them")
-				errors.append("Check the TeX log file, and please let me know via GitHub. Thanks!")
-			continue
-		if "! Emergency stop." in line:
-			state = STATE_SKIP
-			continue
-		# catch over/underfull
-		# skip everything for now
-		# Over/underfull messages end with [] so look for that
-		if line[0:8] in ["Overfull", "Underfull"]:
-			if line[-2:]=="[]": # one-line over/underfull message
-				continue
-			ou_processing = True
-			while ou_processing:
-				try:
-					line = log_iterator.next() # will fail when no more lines
-				except StopIteration:
-					break
-				line_num += 1
-				if len(line)>0 and line[0:3] == " []":
-					ou_processing = False
-			if ou_processing:
-				errors.append("Malformed LOG file: over/underfull")
-				break
-			else:
-				continue
-		line.strip() # get rid of initial spaces
-		# note: in the next line, and also when we check for "!", we use the fact that "and" short-circuits
-		while len(line)>0 and line[0]==')': # denotes end of processing of current file: pop it from stack
-			# files.pop()	
-			if DEBUG:
-				print " "*len(files) + files[-1] + " (%d)" % (line_num,)
-			if files:
-				files.pop()
-			else:
-				errors.append("LaTeXTools cannot correctly detect file names in this LOG file.")
-				errors.append("Please let me know via GitHub. Thanks!")
-				if DEBUG:
-					print "Popping inexistent files"
-				break
-			line = line[1:] # lather, rinse, repeat
-		line.strip() # again, to make sure there is no ") (filename" pattern
-		file_match = file_rx.search(line) # search matches everywhere, not just at the beginning of a line
-		if file_match:
-			file_name = file_match.group(1)
-			# remove quotes
-			if file_name[0] == "\"" and file_name[-1] == "\"":
-				file_name = file_name[1:-1]
-			files.append(file_name)
-			if DEBUG:
-				print " "*len(files) + files[-1] + " (%d)" % (line_num,)
-		if len(line)>0 and line[0]=='!': # Now it's surely an error
-			print line
-			err_msg = line[2:] # skip "! "
-			# next time around, err_msg will be set and we'll extract all info
-			state = STATE_REPORT_ERROR
-			continue
-		warning_match = warning_rx.match(line)
-		if warning_match:
-			# if last character is a dot, it's a single line
-			if line[-1] == '.':
-				handle_warning(line)
-				continue
-			# otherwise, accumulate it
-			current_warning = line
-			state = STATE_REPORT_WARNING
-			continue
-
-	return (errors, warnings)
 
 
 
@@ -335,8 +98,11 @@ class CmdThread ( threading.Thread ):
 				.read().decode(self.caller.encoding, 'ignore') \
 				.encode(sublime_plugin.sys.getdefaultencoding(), 'ignore').splitlines()
 
+		errors = []
+		warnings = []
+
 		try:
-			(errors, warnings) = parseTeXlog(data)
+			(errors, warnings) = parseTeXlog.parse_tex_log(data)
 			content = ["",""]
 			if errors:
 				content.append("There were errors in your LaTeX source") 
@@ -353,12 +119,15 @@ class CmdThread ( threading.Thread ):
 					content.append("However, there were warnings in your LaTeX source") 
 				content.append("")
 				content.extend(warnings)
-		except:
+		except Exception as e:
 			content=["",""]
 			content.append("LaTeXtools could not parse the TeX log file")
 			content.append("(actually, we never should have gotten here)")
+			content.append("")
+			content.append("Python exception: " + repr(e))
+			content.append("")
 			content.append("Please let me know on GitHub. Thanks!")
-		
+
 		self.caller.output(content)
 		self.caller.output("\n\n[Done!]\n")
 		self.caller.finish(len(errors) == 0)
